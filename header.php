65c68aaea5d5d6ebaa9d19f7791bdf31b2f0f866
Michal Stanke <michal@stanke.cz>
2017-07-23 17:26:56 +0200

    Sjednocení vyhledávače

--- header.php
+++ header.php
@@ -3,19 +3,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sk" lang="sk-SK">
 <head profile="http://gmpg.org/xfn/11">
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-  <meta http-equiv="content-language" content="sk" />
+	<meta http-equiv="content-language" content="sk" />
 
 	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Archív správ <?php } ?> <?php wp_title(); ?></title>
 
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
 	<meta name="keywords" content="mozilla, slovakia, slovensko, slovenčina, localization, lokalizácia, lokalizačná komunita, firefox, thunderbird, sunbird, camino, seamonkey, nvu, netscape, rozšírenia, pluginy, doplnky, download, stiahnutie, stiahnuť, fórum, podpora" />
-  <meta name="description" content="Mozilla.sk – základňa slovenskej lokalizačnej komunity"  />
+	<meta name="description" content="Mozilla.sk – základňa slovenskej lokalizačnej komunity"  />
 
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="Články (RSS 2.0)" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Články (Atom 0.3)" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="Komentáre (RSS 2.0)" href="<?php bloginfo('comments_rss2_url'); ?>" />
-<!--	<link rel="alternate" type="application/rss+xml" title="Rozšírenia (RSS 2.0)" href="https://www.mozilla.sk/rozsirenia_rss.php" /> -->
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<link rel="search" type="application/opensearchdescription+xml" title="Vyhľadávací modul pre Mozilla.sk" href="https://www.mozilla.sk/wp-content/download/searchplugins/mozillask_search.xml" />
 	<link rel="icon" href="/favicon.png" type="image/png" />
@@ -30,14 +29,8 @@
 <div id="header">
 		<h1><a href="<?php echo get_settings('home'); ?>" title="<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></h1>
 		<div class="description"><?php bloginfo('description'); ?></div>
-		
-		   <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-
-		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" id="search">
-        <input class="form-text" type="text" size="15" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="keys" />
-        <input id="submit" type="image" src="/wp-content/themes/mozillask/images/searcha.png"  />
-        </form> 
-
-
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		<?php include (TEMPLATEPATH . "/searchform.php"); ?>
 </div>
 <hr />
+
--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
-		<div id="vyhladavanie">
-			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" id="vyhladavanie_form">
-				<input class="form-text" type="text" size="15" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="keys_vyh" />
-				<input id="submit_vyh" type="image" src="/wp-content/themes/mozillask/images/searcha.png"  /><br/>
-			</form>
-		</div>	
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" class="vyhladavanie">
+	<input type="text" size="15" value="<?php echo wp_specialchars($s, 1); ?>" name="s" />
+	<input type="image" src="/wp-content/themes/mozillask/images/searcha.png" />
+</form>
+

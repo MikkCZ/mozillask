5ab1119392f1be56f9df8673b12ac8c38f0fca1e
Michal Stanke <michal@stanke.cz>
2017-07-24 10:32:23 +0200

    Patička

--- footer.php
+++ footer.php
@@ -1,16 +1,13 @@
-
 <hr />
 <div id="footer">
 	<p>
-		Nachádzate sa na stránkach slovenského lokalizačného tímu. © 2004 – <?php echo date('Y')?> Mozilla.sk. Všetky práva vyhradené.
-        <br />
-		Mozilla.sk beží na serveri <a href="http://epicnet.sk/">epicnet.sk</a> na systéme <a href="http://wordpress.org">WordPress</a>. Oficiálne stránky - združenie <a href="http://www.mozilla-europe.org/sk/">Mozilla Europe</a>.
-		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
-		<br />
-		Mozilla, Firefox, Thunderbird sú ochranné známky patriace Mozilla Foundation, &copy; 1998 - <?php echo date('Y')?> Mozilla Foundation.
+		Nachádzate sa na stránkach slovenského lokalizačného tímu. &copy; 2004 – <?php echo date('Y')?> Mozilla.sk. Všetky práva vyhradené.<br/>
+		Mozilla.sk beží na serveri <a href="https://epicnet.sk/">epicnet.sk</a> na systéme <a href="https://wordpress.org">WordPress</a>.<br/>
+		Mozilla, Firefox, Thunderbird sú ochranné známky <a href="https://www.mozilla.org/">Mozilla Foundation</a>.<br/>
 	</p>
 </div>
 </div>
-		<?php do_action('wp_footer'); ?>
+<?php wp_footer(); ?>
 </body>
 </html>
+
